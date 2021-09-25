d056673707dcc9a6259ccbf775e9f709affdefd6
ajrfyd <ajrfyd@naver.com>
2021-09-25 15:45:37 +0900

    Secret update

--- index.js
+++ index.js
@@ -2,14 +2,39 @@ const express = require('express');
 const app = express();
 const  port = 3000;
 const mongoose = require('mongoose');
+const { User } = require('./models/User.js');
+const config = require('./config/key')
+// const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb+srv://ajrfyd3943:3943@boiler.gzbyr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+app.use(express.json())
+
+mongoose.connect(config.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log('Success!!!'))
 .catch(err => console.log(err))
 
 
+
 app.get('/', (req, res) => {
-  res.send('hello???')
+  res.send('Welcome to my page!~')
 })
+
+
+app.post('/register', (req, res) => {
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true,
+    })
+  })
+
+})
+
+
+
+
+
 
 app.listen(port, () => console.log('Listening on ' + port))
